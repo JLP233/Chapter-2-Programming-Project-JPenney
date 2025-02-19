90460c0b87ccfc29b2a2550bf5396403f17ac433
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-02-19 17:45:45 -0500

    Completed Project 1 to display increases in ocean height by years

--- Chapter 2 Programming Project JPenney/Chapter 2 Programming Project JPenney.cpp
+++ Chapter 2 Programming Project JPenney/Chapter 2 Programming Project JPenney.cpp
@@ -2,19 +2,23 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float a = 1.5 * 5;
+    std::cout << "Number of millimeters higher than the current level that the ocean level will be in 5 years:";
+    std::cout << a;
+
+    std::cout << "\n";
+
+    float b = 1.5 * 7;
+    std::cout << "Number of millimeters higher than the current level that the ocean level will be in 7 years:";
+    std::cout << b;
+
+    std::cout << "\n";
+
+    float c = 1.5 * 10;
+    std::cout << "Number of millimeters higher than the current level that the ocean level will be in 10 years:";
+    std::cout << c;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
