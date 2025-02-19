0be6f7152b2b5f82c506282a5c68942e2a103638
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-02-19 18:12:55 -0500

    Removed redundant "std::" from all lines because namespace std is declared at the top

--- Chapter 2 Programming Project JPenney/Chapter 2 Programming Project JPenney.cpp
+++ Chapter 2 Programming Project JPenney/Chapter 2 Programming Project JPenney.cpp
@@ -7,18 +7,18 @@ using namespace std;
 int main()
 {
     float a = 1.5 * 5;
-    std::cout << "Number of millimeters higher than the current level that the ocean level will be in 5 years:";
-    std::cout << a;
+    cout << "Number of millimeters higher than the current level that the ocean level will be in 5 years:";
+    cout << a;
 
-    std::cout << "\n";
+    cout << "\n";
 
     float b = 1.5 * 7;
-    std::cout << "Number of millimeters higher than the current level that the ocean level will be in 7 years:";
-    std::cout << b;
+    cout << "Number of millimeters higher than the current level that the ocean level will be in 7 years:";
+    cout << b;
 
-    std::cout << "\n";
+    cout << "\n";
 
     float c = 1.5 * 10;
-    std::cout << "Number of millimeters higher than the current level that the ocean level will be in 10 years:";
-    std::cout << c;
+    cout << "Number of millimeters higher than the current level that the ocean level will be in 10 years:";
+    cout << c;
 }
