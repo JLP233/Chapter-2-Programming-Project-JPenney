ac1e9b36bd8f95dda327b1a49e0df613fa0f37cd
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-02-24 08:01:52 -0500

    Added code description comment at the top and chaned basic variable names to more specific and descriptive

--- Chapter 2 Programming Project JPenney/Chapter 2 Programming Project JPenney.cpp
+++ Chapter 2 Programming Project JPenney/Chapter 2 Programming Project JPenney.cpp
@@ -1,26 +1,27 @@
 // Chapter 2 Programming Project JPenney.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// 
+//This code calculates the rise in height of ocean water levels by years elapsed in millimeters.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    float a = 1.5 * 5;
+    float OceanHeightIn5Years = 1.5 * 5;
     cout << "Number of millimeters higher than the current level that the ocean level will be in 5 years:";
-    cout << a;
+    cout << OceanHeightIn5Years;
 
     cout << "\n";
 
-    float b = 1.5 * 7;
+    float OceanHeightIn7Years = 1.5 * 7;
     cout << "Number of millimeters higher than the current level that the ocean level will be in 7 years:";
-    cout << b;
+    cout << OceanHeightIn7Years;
 
     cout << "\n";
 
-    float c = 1.5 * 10;
+    float OceanHeightIn10Years = 1.5 * 10;
     cout << "Number of millimeters higher than the current level that the ocean level will be in 10 years:";
-    cout << c;
+    cout << OceanHeightIn10Years;
 
     return 0;
 }
